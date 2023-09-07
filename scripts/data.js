6cdd8a08af56124c7451edf38c070f122270e4ac
Samson Ocran <samson.ocran@gmail.com>
2023-09-07 18:00:51 +0100

    css added and responsived

--- scripts/data.js
+++ scripts/data.js
@@ -2,7 +2,8 @@ const slackInfo = [
     {
         name: 'Samson Ocran',
         picture: 'https://ca.slack-edge.com/T05FFAA91JP-U05RQQ9MU3B-6693fe70f338-512',
-        track: 'Frontend'
+        track: 'Frontend',
+        github: 'https://github.com/jahseed89/slack-info'
     }
 
 ]
--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,12 @@ let slackUserName = document.getElementById('name')
 let currentDayOfTheWeek = document.getElementById('current-day')
 let currentUTCTime = document.getElementById('current-time')
 let myTrack = document.getElementById('track')
+let githubURL = document.getElementById('repo-link')
 
 const slackDataInfo = () => {
 
     // getting the current day
-    const getCurrentDayName = () => {
+    const currentDayName = () => {
         const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         const today = new Date();
         const dayIndex = today.getDay();
@@ -16,7 +17,7 @@ const slackDataInfo = () => {
       }
 
     // getting the current time
-    const getCurrentTime = () => {
+    const currentTime = () => {
         const now = new Date();
         const hours = now.getHours().toString().padStart(2, '0');
         const minutes = now.getMinutes().toString().padStart(2, '0');
@@ -24,13 +25,16 @@ const slackDataInfo = () => {
         
         return `${hours}:${minutes}:${seconds}`;
       }
+      // 
     const user = slackInfo[0]
 
     slackDisplayImage.innerHTML = `<img src= '${user.picture}'/>`
     slackUserName.innerText = user.name
-    currentDayOfTheWeek.innerText = getCurrentDayName()
-    currentUTCTime.innerText = getCurrentTime()
+    currentDayOfTheWeek.innerText = currentDayName()
+    currentUTCTime.innerText = currentTime()
     myTrack.innerText = user.track
+    githubURL.innerHTML = `<a href="${user.github}" target="_blank">Link to Repository</a>`
+
 }
 
 document.addEventListener('DOMContentLoaded', slackDataInfo)
